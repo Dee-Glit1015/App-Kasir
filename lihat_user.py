386b318534be47f0d09af2af6f16e45b1dabc2e5
Desy Permata Sari <119993025+Dee-Glit1015@users.noreply.github.com>
2024-01-21 01:25:47 +0800

    Lihat User Restoran

---
+++ lihat_user.py
@@ -0,0 +1,25 @@
+import pymysql
+
+connection = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='',
+    db='db_restoran',
+)
+
+try:
+    with connection.cursor() as cursor:
+        sql ="SELECT kd_user, nama, no_hp, username, password 'desc' FROM tb_user"
+        try:
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print ("Kode User\tNama\t\tNo HP\t\tUsername\tPassword")
+            print ("--------------------------------------------------------------------------------------")
+            for row in result:
+                print (str (row[0]) + "\t\t" + row [1] + "\t" + row [2]+ "\t" + row [3]+ "\t" + row[4])
+        except:
+            print ("Oops! Something Wrong")
+
+    connection.commit ()
+finally:
+    connection.close()
